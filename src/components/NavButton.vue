4106f26baced4e8c6e79f18e46f1ca78a2b2670b
davidou997 <davidou997@gmail.com>
2023-01-31 17:48:59 -0500

    Initial NavButton

---
+++ src/components/NavButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-btn
+    tile
+    :color="hover ? 'white' : ''"
+    @mouseenter="hover = true"
+    @mouseleave="hover = false">
+    <v-icon
+        v-if="iconName"
+        :color="hover ? hoverColor : defaultColor"
+        class="mr-1">
+        {{ iconName }}
+    </v-icon>
+    <span
+        v-if="text"
+        :class="hover ? 'nav-hover' : 'text'"
+        class="text-none local-font">
+        {{ text }}
+    </span>
+  </v-btn>
+</template>
+
+<script>
+
+export default {
+    props: {
+        iconName: { //Name of MDI icon
+            required: false,
+            type: String
+        },
+        text: { //Text displayed on button
+            required: false,
+            type: String
+        }
+    },
+    data() {
+        return {
+            hover: false //If the button is hovered
+        }
+    },
+    computed: {
+        hoverColor() { //Colour of on-hover effect of button
+            return '#10B77F'
+        },
+        defaultColor() { //Default colour of nav button
+            return '#F5F7FA'
+        }
+    }
+}
+</script>
+
+<style scoped>
+.local-font{
+    font-size: 15px;
+}
+</style>
