64512bf0bf98d7d35872f5f26004f57fc52b7029
davidou997 <davidou997@gmail.com>
2023-01-31 20:13:45 -0500

    Fixed footer length

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -3,9 +3,11 @@
     fixed
     bottom
     padless
+    app
     color="#07090B">
     <v-row
-      justify="center">
+      justify="center"
+      class="pa-4">
       <span style="color:#F5F7FA">This product isn't affiliated with or endorsed by Grinding Gear Games in any way.</span>
     </v-row>
   </v-footer>
