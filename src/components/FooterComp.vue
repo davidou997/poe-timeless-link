953621034d2f75f9b85bc75a2b91b88db38dcf24
davidou997 <davidou997@gmail.com>
2023-01-31 19:50:44 -0500

    Basic footer

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-footer
+    fixed
+    bottom
+    padless
+    color="#07090B">
+    <v-row
+      justify="center">
+      <span style="color:#F5F7FA">This product isn't affiliated with or endorsed by Grinding Gear Games in any way.</span>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
