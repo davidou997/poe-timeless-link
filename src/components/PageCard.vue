a1798bc8dafa1ce0ec53cd28f02d0abd51abef13
davidou997 <davidou997@gmail.com>
2023-01-31 22:35:41 -0500

    Added page card

---
+++ src/components/PageCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card class="page-card">
+    <slot></slot>
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.page-card {
+  background-color: #131A20;
+}
+</style>
