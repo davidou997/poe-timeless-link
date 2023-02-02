fd980d4d564943f458f125bd26d8d423032a049d
davidou997 <davidou997@gmail.com>
2023-02-02 16:34:48 -0500

    Initial jewel title

---
+++ src/components/JewelTitle.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-row class="justify-space-between">
+    <img
+      :src="currentJewelImg"
+      :alt="currentJewelName"
+      class="jewel-img">
+    <span class="text">{{ currentJewelName }}</span>
+    <img
+      :src="currentJewelImg"
+      :alt="currentJewelName"
+      class="jewel-img">
+    
+    
+  </v-row>
+</template>
+
+<script>
+export default {
+  computed: {
+    currentJewel() {
+      return this.$store.getters.getCurrentJewel
+    },
+    currentJewelName() {
+      return this.$store.getters.getJewelName(this.currentJewel)
+    },
+    currentJewelImg() {
+      return this.$store.getters.getJewelImg(this.currentJewel)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.jewel-title {
+  color: #E5E8EB;
+}
+.text {
+  font-size: 30px;
+  font-weight: 500;
+}
+.jewel-img {
+  vertical-align: middle;
+  height: 3em;
+  width: 3em;
+}
+</style>
