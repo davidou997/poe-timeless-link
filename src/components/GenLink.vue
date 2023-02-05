8bf08f7e1e156c48efe09b694604a4105dd06d3b
davidou997 <davidou997@gmail.com>
2023-02-05 14:37:56 -0500

    Initial link gen button

---
+++ src/components/GenLink.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-btn
+    variant="outlined"
+    size="large"
+    @mouseenter="buttonHov = true"
+    @mouseleave="buttonHov = false"
+    @click="genLinks"
+    :class="buttonHov ? 'option-alt-text' : 'option-text'">
+    <v-icon
+        class="mr-1">
+        mdi-link-variant
+    </v-icon>
+    <span
+        class="text-none"
+        :class="buttonHov ? 'text-alt' : ''"
+        >Generate Links</span>
+  </v-btn>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            buttonHov: false
+        }
+    },
+    methods: {
+        genLinks() { //Generates links based on selected criteria
+            //Needs to validate that there is at least one seed and one conqueror
+            this.$store.dispatch('generateLinks')
+        }
+    }
+}
+</script>
+
+<style scoped>
+.option-text {
+  color: #10B77F;
+  background-color: #1D262F;
+}
+.option-alt-text {
+  color: #1D262F;
+  background-color: #10B77F;
+}
+.v-btn {
+    font-size: 15px;
+}
+.text-alt {
+    text-decoration: underline;
+}
+</style>
