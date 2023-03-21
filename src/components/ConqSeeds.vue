d439b62a3891c753c2ab8f6d98254431e2c369b1
davidou997 <davidou997@gmail.com>
2023-03-20 21:49:36 -0400

    Added padding to chip box and fixed variable typo

--- src/components/ConqSeeds.vue
+++ src/components/ConqSeeds.vue
@@ -15,18 +15,29 @@
                 @keydown.enter="submitSeed"/>
         </v-row>
         <v-row>
-            <v-chip
-                v-for="(seed, index) in currentSeeds"
-                :key="index"
-                variant="outlined"
-                closable
-                close-icon="mdi-close-circle"
-                color="#10B77F"
-                class="ma-1"
-                size="large"
-                @click.close="deleteSeed(seed)">
-                <span class="text">{{ seed }}</span>
-            </v-chip>
+            <v-card
+                class="pa-1 pb-3"
+                variant="outlined">
+                <v-card-title>Submitted Seeds</v-card-title>
+                <v-card-subtitle
+                    v-if="noSeeds"><i>Submit a seed above!</i>
+                </v-card-subtitle>
+                <div class="px-3">
+                    <v-chip
+                        v-for="(seed, index) in currentSeeds"
+                        :key="index"
+                        variant="outlined"
+                        closable
+                        close-icon="mdi-close-circle"
+                        color="#10B77F"
+                        class="ma-1"
+                        size="large"
+                        @click.close="deleteSeed(seed)">
+                        <span class="text">{{ seed }}</span>
+                    </v-chip>
+                </div>
+                
+            </v-card>
         </v-row>
     </v-container>
 </template>
@@ -38,7 +49,8 @@ export default {
             currSeed: '', //The current seed
             rules: { //Rules to display error messages for input
                 inRange: value => (value >= this.minSeed && value <= this.maxSeed) || 'Must be within range',
-            }
+            },
+            noSeeds: true
         }
     },
     computed: {
@@ -61,10 +73,18 @@ export default {
             if(this.currSeed && this.currSeed >= this.minSeed && this.currSeed <= this.maxSeed) {
                 this.$store.commit('submitSeed', this.currSeed)
                 this.clearSeed()
+
+                if(this.noSeeds) { //Remove default message when a seed is added
+                    this.noSeeds = false
+                }
             }
         },
         deleteSeed: function(seed) { //Deletes the seed from the chip
             this.$store.commit('deleteSeed', seed)
+            if(this.currentSeeds.size === 0) {
+                console.log(this.currentSeeds.size)
+                this.noSeeds = true
+            }
         },
         clearSeed: function() {
             this.currSeed = ''
@@ -77,5 +97,14 @@ export default {
 .v-text-field {
     font-size: 15px;
     color: #10B77F;
+}
+.v-card {
+    color: #10b77f69;
+    width: 100%;
+    background-color: transparent;
+}
+.v-card-title {
+    font-size: 17px;
+    color: #10B77F
 }
 </style>
