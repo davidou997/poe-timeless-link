2446b611baa67a87a2d832858c20acfadeab0458
davidou997 <davidou997@gmail.com>
2023-03-27 16:12:53 -0400

    Changed title to reflect repo name

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -11,7 +11,7 @@
         @mouseleave="hover = false"
         @click="goHome()">
         <img class="title-img mr-3" :src="jewel" alt="Timeless Jewel">
-        <span :class="hover ? 'nav-hover': 'text'" class="text-none local-font">timeless.bulk</span>
+        <span :class="hover ? 'nav-hover': 'text'" class="text-none local-font">poe-timeless.link</span>
       </v-btn>
     </v-toolbar-title>
     <v-toolbar-items>
